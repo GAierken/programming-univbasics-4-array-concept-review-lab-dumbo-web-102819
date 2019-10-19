0ca93e52c60757ef54938bf69d537c0f4d4cd63b
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 03:10:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,10 @@ def find_element_index(array, value_to_find)
 
     while array.length.times do
           array.index(value_to_find)
-
+    end 
 end
+
+
 
 def find_max_value(array) #array of integers.
   array.length.times do
