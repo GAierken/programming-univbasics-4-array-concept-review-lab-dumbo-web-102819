411a27abfeef6e09fcc815dcf94711afeaaee8db
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 03:06:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,9 @@
 
 
 def find_element_index(array, value_to_find)
-    array.index(value_to_find)
+
     array.length.times do { |index|
-      puts array.index
+      puts array.index(value_to_find)
 
     }
     end
