7d832fdc8a6b53b21798728a0ec411755d902c9b
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 02:44:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,10 +5,10 @@
 
 
 def find_element_index(array, value_to_find)
-    counter=0
-    while counter<array.length do
 
-      counter+=1
+    array.length.times do ||
+
+      
     end
   # Add your solution here
 end
@@ -19,6 +19,6 @@ def find_max_value(array) #array of integers.
 end
 
 def find_min_value(array)
-    while end 
+    while end
   # Add your solution here
 end
