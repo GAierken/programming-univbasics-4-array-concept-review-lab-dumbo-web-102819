73c519a6d6d8b6c701a7d767585a8b559a2fbdd6
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 02:39:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,9 +6,9 @@
 
 def find_element_index(array, value_to_find)
     counter=0
-    while counter<array.length do 
+    while counter<array.length do
       
-    end 
+    end
   # Add your solution here
 end
 
