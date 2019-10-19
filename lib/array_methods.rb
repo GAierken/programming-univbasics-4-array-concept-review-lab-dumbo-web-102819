77d1e1c5eb86acab61d8a1d3c04c6ce455be909e
Guligena Aierken <guligena_aierken@163.com>
2019-10-19 03:12:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,3 @@
-#arrays are sets of **positive integers**.
-#return the _index_ of the value
-#not found, return nil
-#loop over the provided array
-
 
 def find_element_index(array, value_to_find)
 
